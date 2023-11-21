7e6be392db63f4b2f46948035581c96ad61d8e4e
AmbreMandar1812 <ambremandar1812@gmail.com>
2023-11-21 20:34:38 +0530

    http server changes

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const http = require("http");
 const cors = require("cors");
 
 const app = express();
 const isDev = app.settings.env === "development";
+const PORT = process.env.PORT || 3000;
+
+const httpServer = http.createServer(app);
+
 const URL = isDev
-  ? "http://localhost:3000"
+  ? `http://localhost:${PORT}`
   : "https://sketch-board-front-12tvzevvq-2020mandarambre-vesacin.vercel.app";
+
 app.use(cors({ origin: URL }));
-const io = new Server(httpServer, { cors: "http://localhost:3000" });
+
+const io = new Server(httpServer, { cors: { origin: URL } });
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 io.on("connection", (socket) => {
   console.log("server connected");
